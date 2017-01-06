ff712a1a8d381f3e3eaf5e8259b9b721d719f301
Saied <Saied@DESKTOP-IN4646K>
2017-01-06 18:50:09 +0330

    Add project files.

---
+++ directory/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace directory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lstDirectories = new System.Windows.Forms.ListBox();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(50, 6);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(548, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(613, 4);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lstDirectories
+            // 
+            this.lstDirectories.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstDirectories.FormattingEnabled = true;
+            this.lstDirectories.Location = new System.Drawing.Point(15, 54);
+            this.lstDirectories.Name = "lstDirectories";
+            this.lstDirectories.Size = new System.Drawing.Size(673, 134);
+            this.lstDirectories.TabIndex = 3;
+            this.lstDirectories.DoubleClick += new System.EventHandler(this.lstDirectories_DoubleClick);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(15, 220);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(673, 225);
+            this.lstFiles.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Directories:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Files:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(700, 35);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 204);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 260);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 429);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 464);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.lstDirectories);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Directory Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lstDirectories;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ directory/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace directory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPath.Text = System.IO.Directory.GetCurrentDirectory();
+        }
+
+        /// <summary>
+        /// function for get & load directories & files in given path.
+        /// </summary>
+        /// <param name="path"></param>
+        private void GetDirectoryInfo(string path)
+        {
+            //define a directory info object:
+            System.IO.DirectoryInfo oDirectoryInfo = new System.IO.DirectoryInfo(path);
+
+            //clear & load directories & files in lists:
+            lstDirectories.Items.Clear();
+            lstFiles.Items.Clear();
+            if (path.Length > 3) //chek if path is not root to add back link.
+            {
+                lstDirectories.Items.Add("..");
+            }
+
+            foreach (System.IO.DirectoryInfo oCurrentDirectory in oDirectoryInfo.GetDirectories())
+            {
+                lstDirectories.Items.Add(oCurrentDirectory);
+            }
+
+            foreach (System.IO.FileInfo oCurrentFile in oDirectoryInfo.GetFiles())
+            {
+                lstFiles.Items.Add(oCurrentFile);
+            }
+        }
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //check if inputed path to be valid:
+            if (String.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show(
+                                text: "مسیر وارد شده معتبر نمی باشد.", 
+                                caption: "خطا!", 
+                                buttons: MessageBoxButtons.OK, 
+                                defaultButton: MessageBoxDefaultButton.Button1, 
+                                icon: MessageBoxIcon.Error, 
+                                options: MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading
+                               );
+                txtPath.Text = String.Empty;
+                txtPath.Focus();
+                return;
+            }
+
+            //remove brfore & after spaces:
+            txtPath.Text = txtPath.Text.Trim();
+
+            //Call GetDirectoryInfo function:
+            GetDirectoryInfo(txtPath.Text);
+        }
+
+        private void lstDirectories_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstDirectories.SelectedItem != null) //check if selected item to be valid.
+            {
+                //set new path:
+                if (lstDirectories.SelectedItem.ToString() == "..")
+                {
+                    int intLastIndex = txtPath.Text.LastIndexOf("\\");
+                    txtPath.Text = txtPath.Text.Substring(0, intLastIndex);
+                }
+                else
+                {
+                    txtPath.Text = string.Format("{0}\\{1}", txtPath.Text, lstDirectories.SelectedItem.ToString());
+                }
+
+                //Call GetDirectoryInfo function
+                GetDirectoryInfo(txtPath.Text);
+            }
+        }
+    }
+}
